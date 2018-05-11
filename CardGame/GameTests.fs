d1cc4f8f685d4147d1da0930b1fa39f0958df5b2
pawelwicher <pawel.wicher@op.pl>
2018-05-11 06:52:46 +0200

    Test on cards equality added

--- CardGame/GameTests.fs
+++ CardGame/GameTests.fs
@@ -1,13 +1,13 @@
-namespace CardGame.Tests
+namespace CardGameTests
 
 open System
 open Xunit
 open CardGame.Game
 
-module CardTests =
+module CardGameTests =
 
     [<Fact>]
-    let ``Create 'Knight' card`` () =
+    let ``Create 'Knight' card.`` () =
         let expected = {
             id = Knight
             name = "Knight"
@@ -22,14 +22,22 @@ module CardTests =
         Assert.Equal(expected, actual)
 
     [<Fact>]
-    let ``Calculated score of the empty board should be 0 : 0`` () =
+    let ``Create two 'Knight' cards, they should be equal but not the same instance.`` () =        
+        let knight1 = createCard Knight
+        let knight2 = createCard Knight
+
+        Assert.Equal(knight1, knight2)
+        Assert.NotSame(knight1, knight2)
+
+    [<Fact>]
+    let ``Calculated score of the empty board should be 0 : 0.`` () =
         let board = createBoard()
         let actual = calculateScore board
 
         Assert.Equal((0, 0), actual)
 
     [<Fact>]
-    let ``Calculated score of the board with a 'Knight' put on the north should be 15 : 0`` () =
+    let ``Calculated score of the board with a 'Knight' put on the north should be 15 : 0.`` () =
         let board = createBoard()
         let knight = createCard Knight
         board.north.melee1.[5] <- Some knight
@@ -39,7 +47,7 @@ module CardTests =
         Assert.Equal((15, 0), actual)
 
     [<Fact>]
-    let ``Calculated score of the board with a 'Knight' put on the south should be 0 : 15`` () =
+    let ``Calculated score of the board with a 'Knight' put on the south should be 0 : 15.`` () =
         let board = createBoard()
         let knight = createCard Knight
         board.south.melee1.[5] <- Some knight
@@ -49,7 +57,7 @@ module CardTests =
         Assert.Equal((0, 15), actual)
 
     [<Fact>]
-    let ``Calculated score of the board with a 'Knight' put on the north and south should be 15 : 15`` () =
+    let ``Calculated score of the board with a 'Knight' put on the north and south should be 15 : 15.`` () =
         let board = createBoard()
         let knight1 = createCard Knight
         let knight2 = createCard Knight
@@ -61,7 +69,7 @@ module CardTests =
         Assert.Equal((15, 15), actual)
 
     [<Fact>]
-    let ``Calculated score of the board with 2 'Knight' unit on the north and one on the south should be 30 : 15`` () =
+    let ``Calculated score of the board with two 'Knight' units on the north and one on the south should be 30 : 15.`` () =
         let board = createBoard()
         let knight1 = createCard Knight
         let knight2 = createCard Knight
