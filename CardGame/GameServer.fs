acfd1d33951a29ff7add3a33b9c4402b00dbb8db
pawelwicher <34919112+pawelwicher@users.noreply.github.com>
2018-05-16 16:55:49 +0200

    Update GameServer.fs

--- CardGame/GameServer.fs
+++ CardGame/GameServer.fs
@@ -1,4 +1,4 @@
-﻿namespace CardGame
+namespace CardGame
 
 open System
 open System.Net.Sockets
@@ -6,25 +6,34 @@ open SimpleTCP
 
 module GameServer =
 
+    let playersNames = ["bob"; "ted"]
+    let mutable i = 0
+
     let start() =
         let simpleTcpServer = new SimpleTcpServer()
         let server = simpleTcpServer.Start(port = 8080)
-        let clients = Map.empty
-        let clear = Console.Clear
         let send (msg : string) (client : TcpClient) =
             let data = server.StringEncoder.GetBytes(msg)
             client.GetStream().Write(data, 0, data.Length)
-        
-        clear()
+
+        Console.Clear()
         "Server started." |> Console.WriteLine
 
-        server.ClientConnected.Add(fun _ -> "Client connected." |> Console.WriteLine)
+        server.ClientConnected.Add(fun c ->
+            "Client connected." |> Console.WriteLine
+            let name = playersNames.[i]
+            send ("Hello : " + name) c
+            i <- i + 1
+        )
 
         server.ClientDisconnected.Add(fun _ -> "Client disconnected." |> Console.WriteLine)
 
         server.DataReceived.Add(fun msg ->
-            msg.MessageString |> Console.WriteLine
-            msg.Reply("<- hello")
+            let cmd = msg.MessageString
+            cmd |> Console.WriteLine
+            
+            // here todo: processing cmd. bob card1 S_A5 N_A5,N_A6
+
             //let data = server.StringEncoder.GetBytes(e.MessageString)
             //list.ForEach(fun c -> c.GetStream().Write(data, 0, data.Length))
         )
