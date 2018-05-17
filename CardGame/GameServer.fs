fca40ce89b00cad21a40a4a317087a67c005c51d
pawelwicher <34919112+pawelwicher@users.noreply.github.com>
2018-05-17 15:55:33 +0200

    Update GameServer.fs

--- CardGame/GameServer.fs
+++ CardGame/GameServer.fs
@@ -3,39 +3,61 @@ namespace CardGame
 open System
 open System.Net.Sockets
 open SimpleTCP
+open Game
 
 module GameServer =
 
-    let playersNames = ["aaa"; "bbb"; "ccc"; "ddd"; "eee"]
-    let mutable i = 0
+    let mutable players : Player list = []
+    let mutable games : Game list = []
 
     let start() =
         let simpleTcpServer = new SimpleTcpServer()
         let server = simpleTcpServer.Start(port = 8080)
-        let send (msg : string) (client : TcpClient) =
+
+        let send (msg : string) (client : TcpClient) : unit =
             let data = server.StringEncoder.GetBytes(msg)
             client.GetStream().Write(data, 0, data.Length)
+
+        let gameToString (game : Game) : string =
+            game.name + " state: " + game.state
+        
+        let gameSendToPlayers (game : Game) : unit =
+            send (gameToString game) game.player1.tcpClient
+            send (gameToString game) game.player2.tcpClient
+
+        let findGame (games : Game list) (client : TcpClient) : Game option =
+            let games = games |> List.filter (fun (x : Game) -> x.player1.tcpClient.Client.Handle = client.Client.Handle || x.player2.tcpClient.Client.Handle = client.Client.Handle)
+            if games.Length = 1 then
+                Some games.[0]
+            else
+                None
 
         Console.Clear()
         "Server started." |> Console.WriteLine
 
-        server.ClientConnected.Add(fun c ->
+        server.ClientConnected.Add(fun client ->
             "Client connected." |> Console.WriteLine
-            let name = playersNames.[i]
-            send ("Hello: " + name) c
-            i <- i + 1
-        )
+            let player = { tcpClient = client }
+            players <- player :: players
 
-        server.ClientDisconnected.Add(fun _ -> "Client disconnected." |> Console.WriteLine)
+            if players.Length = 2 then
+                let game = { player1 = players.[0]; player2 = players.[1]; name = ("Game " + games.Length.ToString()); state = "" }
+                games <- game :: games
+                players <- []
+                gameSendToPlayers game
+        )
 
         server.DataReceived.Add(fun msg ->
             let cmd = msg.MessageString
             cmd |> Console.WriteLine
-            
-            // here todo: processing cmd. bob card1 S_A5 N_A5,N_A6
 
-            //let data = server.StringEncoder.GetBytes(e.MessageString)
-            //list.ForEach(fun c -> c.GetStream().Write(data, 0, data.Length))
+            let game = findGame games msg.TcpClient
+
+            if game.IsSome then
+                game.Value.state <- cmd
+                gameSendToPlayers game.Value
+
+            // here todo: processing cmd. bob card1 S_A5 N_A5,N_A6
         )
 
         Console.ReadKey() |> ignore
